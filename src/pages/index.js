ec93c9b93b1ab9c2a67b7687769341e096a5b12b
Adia Holt <adia.holt@gmail.com>
2025-01-06 21:39:53 -0600

    Setup components and backend connection

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
   return (
   <div>
     <h1>Coffee Shop Frontend Splash Page</h1>
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>
+    <button className="btn btn-primary">Button</button>
   </div>
   )
 }
