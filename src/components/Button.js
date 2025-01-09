f26d3410dc7e7531659ba1850051f4d981117ac7
Adia Holt <adia.holt@gmail.com>
2025-01-08 20:44:33 -0600

    created product page

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 // components/Button.js
 import PropTypes from 'prop-types';
 export default function Button({ label }) {
-  return <button className="btn btn-primary">{label}</button>;
+  return <button onClick={handleClick} className="btn btn-primary">{label}</button>;
 }
 Button.propTypes = {
   label: PropTypes.string.isRequired,
--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -1,12 +1,27 @@
 import { useRouter } from 'next/router';
+import products from '../../mocks/products.json';
+import Button from '@/components/Button';
 
 export default function ProductPage() {
   // For pages with dynamic routes, you can use the useRouter hook to get the route params
   const router = useRouter();
-  const { id } = router.query; // Get 'id' from the route params
+  const id  = router.query; // Get 'id' from the route params
+  const product = products[id];
   return (
-    <div>
-      <h1>Product Page for product &#35; { id }</h1>
+    <div className="card glass w-96">
+      <figure>
+        <img>
+        src="{product.imageUrl}"
+        alt="{product.name}"
+        </img>
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{product.name}</h2>
+        <p>${product.price}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Add to Cart</button>
+        </div>
+      </div>
     </div>
   );
 }
